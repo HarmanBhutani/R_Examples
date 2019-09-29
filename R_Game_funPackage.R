6c61f28b5e4dc723ea56f9fc8eba30d6132b9cd9
HarmanBhutani <byteharman0101@gmail.com>
2019-09-29 05:14:34 +0000

    Mineswepper game in R

--- R_Game_funPackage.R
+++ R_Game_funPackage.R
@@ -1,3 +1,14 @@
 install.packages("fun")
 library(fun)
 
+demo(package="fun")
+
+mine_sweeper()
+?mine_sweeper
+
+
+if (interactive()) {
+  if (.Platform$OS.type == "windows") 
+    x11() else x11(type = "Xlib")
+  mine_sweeper()
+}
