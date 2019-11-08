85c008fa61339886e648fba2d472bb0ed66f3571
HarmanBhutani <byteharman0101@gmail.com>
2019-11-08 22:59:18 +0000

    Development of Simple Neural Network in R

---
+++ NN_R.R
@@ -0,0 +1,31 @@
+install.packages("neuralnet")
+
+# creating training data set
+TKS=c(20,10,30,20,80,30)
+CSS=c(90,20,40,50,50,80)
+Placed=c(1,0,0,0,1,1)
+# Here, you will combine multiple columns or features into a single set of data
+df=data.frame(TKS,CSS,Placed)
+
+require(neuralnet)
+
+# fit neural network
+nn=neuralnet(Placed~TKS+CSS,data=df, hidden=3,act.fct = "logistic",
+             linear.output = FALSE)
+
+plot(nn)
+
+# creating test set
+TKS=c(30,40,85)
+CSS=c(85,50,40)
+test=data.frame(TKS,CSS)
+
+Predict=compute(nn,test)
+Predict$net.result
+
+nn$result.matrix
+
+# Converting probabilities into binary classes setting threshold level 0.5
+prob <- Predict$net.result
+pred <- ifelse(prob>0.5, 1, 0)
+pred
