fd1c907e1b6e2cf355b2f230300189b86552e2a1
Harman Bhutani <byteharman0101@gmail.com>
2019-11-12 14:10:47 -0500

    Update KMeans_R.R

--- KMeans_R.R
+++ KMeans_R.R
@@ -6,10 +6,7 @@ summary(iris)
 head(iris)
 Iris.features = iris
 Iris.features$class <- NULL
-view(Iris.features)
 input<- Iris.features[,1:4]
-input
 results <- kmeans(input,centers = 5, nstart = 20)
-results
 plotcluster(input, results$cluster)
 
